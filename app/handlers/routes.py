06bc091f054ad6204d482e20da943351ad1d41c1
Yuqi Gong <yuqigong@andrew.cmu.edu>
2022-11-08 22:46:35 -0500

    Implemented the predict function, added error handling for missing/invalid parameters

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -1,3 +1,4 @@
+import json
 import this
 from flask import Flask, jsonify, request
 import joblib
@@ -18,12 +19,34 @@ def configure_routes(app):
 
     @app.route('/predict')
     def predict():
-        #use entries from the query string here but could also use json
-        G1 = request.args.get('G1')
-        G2 = request.args.get('G2')
-        Failures = request.args.get('Failures')
-        Higher = request.args.get('Higher')
-        data = [[G1], [G2], [Failures], [Higher]]
+        #use entries from the query string here 
+        try:
+            if (len(request.args) != 0): 
+                G1 = request.args['G1']
+                G2 = request.args['G2']
+                Failures = request.args['Failures']
+                Higher = request.args['Higher']
+            # entries from json
+            else: 
+                response = json.loads(request.json)
+                print(response)
+                G1 = response['G1']
+                G2 = response['G2']
+                Failures = response['Failures']
+                Higher = response['Higher']
+        except Exception as e:
+            print(e)
+            return 'Missing value', 404
+
+        if (int(G1) < 0 or int(G1) > 20):
+            return 'Invalid G1 value', 400
+        if (int(G2) < 0 or int(G2) > 20):
+            return 'Invalid G2 value', 400
+        if (int(Failures) < 1 or int(Failures) > 4):
+            return 'Invalid Failures value', 400
+        if (not (isinstance(Higher, bool) or (Higher == "False" or Higher == "True"))):
+            return 'Invalid Higher value', 400
+
         query_df = pd.DataFrame({
             'G1': pd.Series(G1),
             'G2': pd.Series(G2),
@@ -33,6 +56,7 @@ def configure_routes(app):
         query = pd.get_dummies(query_df)
         prediction = clf.predict(query)
         prediction = np.ndarray.item(prediction)
+        
         category = ''
         if (prediction == 1):
             category = 'average'
@@ -42,4 +66,5 @@ def configure_routes(app):
             category = 'exemplar'
         else:
             return 'Invalid prediction calculated', 400
+        
         return jsonify(category)
